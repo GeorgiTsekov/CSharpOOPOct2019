4a543a9685d08d6eead28aae8dcfbb09be008504
GeorgiTsekov <53428844+GeorgiTsekov@users.noreply.github.com>
2020-07-08 18:45:36 +0300

    Add Exams 2019

---
+++ Exams/01. Structure_Skeleton/PlayersAndMonsters/Repositories/CardRepository.cs
@@ -0,0 +1,66 @@
+﻿namespace PlayersAndMonsters.Repositories
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using PlayersAndMonsters.Models.Cards.Contracts;
+    using PlayersAndMonsters.Repositories.Contracts;
+
+    public class CardRepository : ICardRepository
+    {
+        private List<ICard> cards;
+
+        public CardRepository()
+        {
+            this.cards = new List<ICard>();
+        }
+
+        public int Count => this.cards.Count;
+
+        public IReadOnlyCollection<ICard> Cards => cards.AsReadOnly();
+
+        public void Add(ICard card)
+        {
+            if (card == null)
+            {
+                throw new ArgumentException("Card cannot be null!");
+            }
+
+            if (this.cards.Any(c => c.Name == card.Name))
+            {
+                throw new ArgumentException($"Card {card.Name} already exists!");
+            }
+
+            this.cards.Add(card);
+        }
+
+        public bool Remove(ICard card)
+        {
+            if (card == null)
+            {
+                throw new ArgumentException("Card cannot be null!");
+            }
+
+            bool result = false;
+
+            if (this.cards.Contains(card))
+            {
+                this.cards.Remove(card);
+                result = true;
+            }
+
+            return result;
+        }
+
+        public ICard Find(string name)
+        {
+            if (this.cards.Any(c => c.Name == name))
+            {
+                return cards.FirstOrDefault(c => c.Name == name);
+            }
+
+            return null;
+        }
+
+    }
+}
---
+++ Exams/01. Structure_Skeleton/PlayersAndMonsters/Repositories/PlayerRepository.cs
@@ -0,0 +1,66 @@
+﻿namespace PlayersAndMonsters.Repositories
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using PlayersAndMonsters.Models.Players.Contracts;
+    using PlayersAndMonsters.Repositories.Contracts;
+
+    public class PlayerRepository : IPlayerRepository
+    {
+        private List<IPlayer> players;
+
+        public PlayerRepository()
+        {
+            this.players = new List<IPlayer>();
+        }
+
+        public int Count => this.players.Count;
+
+        public IReadOnlyCollection<IPlayer> Players => this.players.AsReadOnly();
+
+        public void Add(IPlayer player)
+        {
+            if (player == null)
+            {
+                throw new ArgumentException("Player cannot be null");
+            }
+
+            if (this.players.Any(p => p.Username == player.Username))
+            {
+                throw new ArgumentException($"Player {player.Username} already exists!");
+            }
+
+            this.players.Add(player);
+        }
+
+        public bool Remove(IPlayer player)
+        {
+            if (player == null)
+            {
+                throw new ArgumentException("Player cannot be null");
+            }
+
+            bool result = false;
+
+            if (this.players.Contains(player))
+            {
+                this.players.Remove(player);
+                result = true;
+            }
+
+            return result;
+        }
+
+        public IPlayer Find(string username)
+        {
+            if (this.players.Any(p => p.Username == username))
+            {
+                return this.players.FirstOrDefault(p => p.Username == username);
+            }
+
+            return null;
+        }
+
+    }
+}
