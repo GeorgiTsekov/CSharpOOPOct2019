3758b967813e828e41af6f4bc7708b318aba63b3
GeorgiTsekov <53428844+GeorgiTsekov@users.noreply.github.com>
2020-07-08 18:42:58 +0300

    Add Exams 2019

---
+++ Exams/01. Structure_Skeleton/Models/Mission/Mission.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using SpaceStation.Models.Astronauts.Contracts;
+using SpaceStation.Models.Planets;
+
+namespace SpaceStation.Models.Mission
+{
+    public class Mission : IMission
+    {
+        public void Explore(IPlanet planet, ICollection<IAstronaut> astronauts)
+        {
+            List<IAstronaut> explorers = astronauts.Where(a => a.Oxygen > 0).ToList();
+
+            foreach (var astronaut in explorers)
+            {
+                while (astronaut.Oxygen > 0 && planet.Items.Count > 0)
+                {
+                    string item = planet.Items.FirstOrDefault();
+
+                    astronaut.Bag.Items.Add(item);
+
+                    astronaut.Breath();
+
+                    planet.Items.Remove(item);
+                }
+            }
+        }
+    }
+}
---
+++ Exams/01. Structure_Skeleton/Models/Planets/Planet (2).cs
@@ -0,0 +1,34 @@
+﻿namespace SpaceStation.Models.Planets
+{
+    using SpaceStation.Models.Bags;
+    using System;
+    using System.Collections.Generic;
+
+    public class Planet : IPlanet
+    {
+        private IBag items;
+        private string name;
+
+        public Planet(string name)
+        {
+            this.Name = name;
+            this.items = new Backpack();
+        }
+
+        public ICollection<string> Items => this.items.Items;
+
+        public string Name
+        {
+            get => this.name;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentNullException("Invalid name!");
+                }
+
+                this.name = value;
+            }
+        }
+    }
+}
