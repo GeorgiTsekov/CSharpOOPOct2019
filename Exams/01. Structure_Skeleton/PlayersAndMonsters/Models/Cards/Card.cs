1d68be16f8a085390b84bb5d709568f1b4cacb0b
GeorgiTsekov <53428844+GeorgiTsekov@users.noreply.github.com>
2020-07-08 18:45:21 +0300

    Add Exams 2019

---
+++ Exams/01. Structure_Skeleton/PlayersAndMonsters/Models/Cards/Card.cs
@@ -0,0 +1,61 @@
+﻿namespace PlayersAndMonsters.Models.Cards
+{
+    using PlayersAndMonsters.Models.Cards.Contracts;
+    using System;
+
+    public abstract class Card : ICard
+    {
+        private string _name;
+        private int _damagePoints;
+        private int _healthPoints;
+
+        protected Card(string name, int damagePoints, int healthPoints)
+        {
+            this.Name = name;
+            this.DamagePoints = damagePoints;
+            this.HealthPoints = healthPoints;
+        }
+
+        public string Name
+        {
+            get => _name;
+            private set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException("Card's name cannot be null or an empty string.");
+                }
+
+                _name = value;
+            }
+        }
+
+        public int DamagePoints
+        {
+            get => _damagePoints;
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Card's damage points cannot be less than zero.");
+                }
+
+                _damagePoints = value;
+            }
+        }
+
+        public int HealthPoints
+        {
+            get => _healthPoints;
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Card's HP cannot be less than zero.");
+                }
+
+                _healthPoints = value;
+            }
+        }
+    }
+}
---
+++ Exams/01. Structure_Skeleton/PlayersAndMonsters/Models/Cards/MagicCard.cs
@@ -0,0 +1,15 @@
+﻿using PlayersAndMonsters.Models.Cards.Contracts;
+
+namespace PlayersAndMonsters.Models.Cards
+{
+    public class MagicCard : Card, ICard
+    {
+        private const int DamagePointsConst = 5;
+        private const int HealthPointsConst = 80;
+
+        public MagicCard(string name) 
+            : base(name, DamagePointsConst, HealthPointsConst)
+        {
+        }
+    }
+}
